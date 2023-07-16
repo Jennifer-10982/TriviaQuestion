53c3c7471a25460b6dcb9e4ca47ed6c51ebc4d9d
Jennifer Huynh <jphuy.12@gmail.com>
2023-07-16 18:38:52 -0400

    Finished Welcome Page Design Only

--- app/src/main/java/algonquin/cst2335/triviaquestion/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/triviaquestion/MainActivity.java
@@ -3,6 +3,8 @@ package algonquin.cst2335.triviaquestion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        * Reference: https://code.tutsplus.com/how-to-add-a-dropdown-menu-in-android-studio--cms-37860t
+        * */
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence>myAdapter = ArrayAdapter.createFromResource(this, R.array.categories, android.R.layout.simple_spinner_item);
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(myAdapter);
+
+
     }
 }
