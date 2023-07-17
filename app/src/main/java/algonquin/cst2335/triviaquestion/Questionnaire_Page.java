df39c4abfa303401f43de1a8a343e87f8ce6ee65
Jennifer Huynh <jphuy.12@gmail.com>
2023-07-17 00:33:09 -0400

    Commit: Layout finished. Working on recyclerView as of now

--- app/src/main/java/algonquin/cst2335/triviaquestion/Questionnaire_Page.java
+++ app/src/main/java/algonquin/cst2335/triviaquestion/Questionnaire_Page.java
@@ -1,10 +1,12 @@
 package algonquin.cst2335.triviaquestion;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import algonquin.cst2335.triviaquestion.databinding.ActivityMainBinding;
 import algonquin.cst2335.triviaquestion.databinding.ActivityQuestionnairePageBinding;
@@ -32,13 +34,18 @@ public class Questionnaire_Page extends AppCompatActivity {
 
         variableBinding.points.setText(model.display_points);
 
+        /**Checks whether the answer is correct. If it is, then the points increase. If not, then points decrease.
+         * A message is display to notify whether the answer is correct or not.*/
         binding.submit.setOnClickListener(clk ->{
             if (answer == true){
                 counter = counter + 30;
+                /**Converting the integer into a String*/
                 variableBinding.points.setText(Integer.toString(counter));
+                Toast.makeText(this, "Correct!", Toast.LENGTH_SHORT).show();
             }else{
                 counter = counter - 10;
                 variableBinding.points.setText(Integer.toString(counter));
+                Toast.makeText(this, "Incorrect", Toast.LENGTH_SHORT).show();
             };
         });
     }
